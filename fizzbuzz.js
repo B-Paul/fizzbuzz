de01d30587f8468bd8fcacbda316c64419989d5c
B Paul Anderson <benpaul.anderson@gmail.com>
2015-01-30 17:12:37 -0600

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var limit, body, term;
+  limit = 100;
+  body = $(document.body);
+
+  for (var i = 1; i <= limit; i += 1) {
+    term = ''
+      + (i % 3 === 0 ? 'fizz ' : '')
+      + (i % 5 === 0 ? 'buzz ' : '')
+      || i.toString();
+    body.append('<div>' + term.trim() + '</div>');
+  }
+})
